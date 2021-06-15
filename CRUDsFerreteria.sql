fc59fc393b8d9c7480de2cdfa82842725fd4c2c3
Kaled Sánchez <kaledsv@gmail.com>
2021-06-14 21:57:42 -0600

    usuarios y contraseñas para web

--- CRUDsFerreteria.sql
+++ CRUDsFerreteria.sql
@@ -27,6 +27,8 @@ CREATE TABLE `cliente` (
   `nombre` varchar(45) NOT NULL,
   `apellido` varchar(45) NOT NULL,
   `tarjeta` varchar(45) NOT NULL,
+  `user` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
