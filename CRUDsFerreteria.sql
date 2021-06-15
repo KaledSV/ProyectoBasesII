445c6c0a91a9a2f7f503de853dc86cfbde61a836
Kaled Sánchez <kaledsv@gmail.com>
2021-06-14 22:07:44 -0600

    cambio por palabra reservada

--- CRUDsFerreteria.sql
+++ CRUDsFerreteria.sql
@@ -28,7 +28,7 @@ CREATE TABLE `cliente` (
   `apellido` varchar(45) NOT NULL,
   `tarjeta` varchar(45) NOT NULL,
   `user` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
+  `pass` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
