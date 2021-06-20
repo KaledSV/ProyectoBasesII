f8b9fd62ffc992ba9a0adbe4c4c6db38ec721557
Aozhen <aozhen01@gmail.com>
2021-06-19 20:01:46 -0600

    admin preparacion

--- Admin.aspx
+++
@@ -1,3 +0,0 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Admin.aspx.cs" Inherits="BD_Proyecto.Admin" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-</asp:Content>
--- Administracion/MainAdmin.aspx.designer.cs
+++ Administracion/MainAdmin.aspx.designer.cs
@@ -7,20 +7,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BD_Proyecto
+namespace BD_Proyecto.Administracion
 {
 
 
-    public partial class WebForm2
+    public partial class MainAdmin
     {
 
         /// <summary>
-        /// Login1 control.
+        /// form1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Login Login1;
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// LinkButton1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
     }
 }
