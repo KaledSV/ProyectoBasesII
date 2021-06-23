24db9c7aed3744d576a1fca5ad3365b46ee03837
Irsacfac <irsacfac@gmail.com>
2021-06-23 00:47:49 -0600

    Update CRUDsFerreteria.sql

--- CRUDsFerreteria.sql
+++ CRUDsFerreteria.sql
@@ -445,6 +445,41 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `getDirecciones` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getDirecciones`()
+BEGIN
+	DROP TEMPORARY TABLE IF EXISTS TempDirecciones;
+	create temporary table  TempDirecciones (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+										ejeX FLOAT,
+                                        ejeY FLOAT);
+	
+    INSERT INTO TempDirecciones(ejeX,
+								ejeY)
+	SELECT ST_X(d.localizacion),
+			ST_Y(d.localizacion)
+    FROM entrega e
+    INNER JOIN venta v ON v.id = e.id_venta
+    INNER JOIN direccion d ON d.id_cliente = v.id_cliente
+    WHERE e.entregado = 0;
+        
+	SELECT ejeX,
+			ejeY
+    FROM TempDirecciones;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `mejores_ultimos_meses` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -937,4 +972,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-22 21:45:18
+-- Dump completed on 2021-06-23  0:47:38
